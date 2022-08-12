fc77327c6fc2d366f6c8ed2f7b472353c6b43e0b
alachhman <33943685+alachhman@users.noreply.github.com>
2022-08-12 19:57:23 -0400

    Added Character creator and firebase connectivity

--- apipractice/index.js
+++ apipractice/index.js
@@ -1,21 +1,44 @@
 const express = require('express'), http = require('http');
 const cors = require('cors')
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./serviceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 const hostname = 'localhost';
 const port = 8080;
 const app = express();
 app.use(cors())
-
 const sample_server = http.createServer(app);
+
+let db = admin.firestore();
 
 sample_server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-app.get('/test/:name?', function (req, res) {
-    console.log("/monkey");
-    let name = req.query.name;
-    res.send( '<h1>hello ' + name + '</h1>');
+app.get('/testWrite', async function (req, res) {
+    const docRef = db.collection('users').doc('alovelace');
+
+    await docRef.set({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815
+    });
+
+    res.send('<h1>sent data</h1>')
+});
+
+app.get('/testRead', async function (req, res) {
+    const snapshot = await db.collection('users').get();
+    snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+    });
+
+    res.json(snapshot)
 });
 
 app.get('/add/:n1?/:n2?', function (req, res) {
@@ -24,6 +47,57 @@ app.get('/add/:n1?/:n2?', function (req, res) {
     let number2 = req.query.n2;
     let number3 = parseInt(number1) + parseInt(number2);
     res.json( {result: number3});
+});
+
+app.get('/newCharacter/:name?', async function (req, res) {
+    const docRef = db.collection('character').doc(req.query.name);
+
+    let character = {
+        name: req.query.name,
+        level: 10,
+        currHp: 100,
+        baseStats: {
+            atk: Math.random() * 10,
+            def: Math.random() * 10,
+            spd: Math.random() * 10,
+            eva: Math.random() * 10,
+            maxHp: (Math.random() * 100) + 100,
+        },
+        growthRate: {
+            atk: Math.random(),
+            def: Math.random(),
+            spd: Math.random(),
+            eva: Math.random(),
+            maxHp: Math.random(),
+        },
+        stats: {
+            atk: 0,
+            def: 0,
+            spd: 0,
+            eva: 0,
+            maxHp: 0,
+        }
+    }
+
+    let generateStat = (baseStat, growthrate, level) => {
+        let stat = baseStat;
+        for (let i = 0; i < level; i++) {
+            if (Math.random() < growthrate) {
+                stat++;
+            }
+        }
+        return stat;
+    }
+
+    character.stats.atk = Math.floor(generateStat(character.baseStats.atk, character.growthRate.atk, character.level))
+    character.stats.def = Math.floor(generateStat(character.baseStats.def, character.growthRate.def, character.level))
+    character.stats.spd = Math.floor(generateStat(character.baseStats.spd, character.growthRate.spd, character.level))
+    character.stats.eva = Math.floor(generateStat(character.baseStats.eva, character.growthRate.eva, character.level))
+    character.stats.maxHp = Math.floor(generateStat(character.baseStats.maxHp, character.growthRate.maxHp, character.level))
+
+    await docRef.set(character);
+
+    res.send(character)
 });
 
 app.get('/', function (req, res) {
--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -3,41 +3,118 @@ import './App.css';
 import React, {useEffect} from "react";
 
 function App() {
-    const [numbers, setNumbers] = React.useState({n1: 0, n2: 0});
-    const [answer, setAnswer] = React.useState({result: 0});
+    // const [numbers, setNumbers] = React.useState({n1: 0, n2: 0});
+    // const [answer, setAnswer] = React.useState({result: 0});
+    //
+    // useEffect(() => {
+    //     console.log("Api called")
+    //     fetchApi()
+    // }, [numbers])
+    //
+    // const fetchApi = async () => {
+    //     fetch('http://localhost:8080/add?n1=' + numbers.n1 + "&n2=" + numbers.n2)
+    //         .then(response => response.json())
+    //         .then(data => setAnswer(data));
+    // }
+    //
+    // const changeFirst = (event) => {
+    //     console.log(event.target.value);
+    //     setNumbers({n1: parseInt(event.target.value), n2: numbers.n2});
+    // }
+    //
+    // const changeSecond = (event) => {
+    //     console.log(event.target.value);
+    //     setNumbers({n1: numbers.n1, n2: parseInt(event.target.value)});
+    // }
+    //
+    // return (
+    //     <div className="App">
+    //         <header className="App-header">
+    //             <img src={logo} className="App-logo" alt="logo"/>
+    //             <p>
+    //                 Choose two numbers to add:
+    //             </p>
+    //             n1: <input id="n1" type={"number"} onChange={changeFirst}></input>
+    //             n2: <input id="n2" type={"number"} onChange={changeSecond}></input>
+    //             <br/>
+    //             <h2 className={"result"}>Result: {answer.result}</h2>
+    //         </header>
+    //     </div>
+    // );
+
+    const [name, setName] = React.useState("Name");
+    const [character, setCharacter] = React.useState(
+        {
+            name: name,
+            level: 10,
+            currHp: 100,
+            baseStats: {
+                atk: Math.random() * 10,
+                def: Math.random() * 10,
+                spd: Math.random() * 10,
+                eva: Math.random() * 10,
+                maxHp: (Math.random() * 100) + 100,
+            },
+            growthRate: {
+                atk: Math.random(),
+                def: Math.random(),
+                spd: Math.random(),
+                eva: Math.random(),
+                maxHp: Math.random(),
+            },
+            stats: {
+                atk: 0,
+                def: 0,
+                spd: 0,
+                eva: 0,
+                maxHp: 0,
+            }
+        }
+    );
 
     useEffect(() => {
         console.log("Api called")
         fetchApi()
-    }, [numbers])
+    }, [])
 
     const fetchApi = async () => {
-        fetch('http://localhost:8080/add?n1=' + numbers.n1 + "&n2=" + numbers.n2)
+        fetch('http://localhost:8080/newCharacter?name=' + name)
             .then(response => response.json())
-            .then(data => setAnswer(data));
+            .then(data => setCharacter(data));
     }
 
-    const changeFirst = (event) => {
+    const changeName = (event) => {
         console.log(event.target.value);
-        setNumbers({n1: parseInt(event.target.value), n2: numbers.n2});
+        setName(event.target.value);
     }
 
-    const changeSecond = (event) => {
-        console.log(event.target.value);
-        setNumbers({n1: numbers.n1, n2: parseInt(event.target.value)});
+    const PrettyPrintStats = (props) => {
+        let stats = props.stats;
+        return (
+            <div>
+                <p>HP: {stats.maxHp + "\n"}</p>
+                <p>ATK: {stats.atk + "\n"}</p>
+                <p>DEF: {stats.def + "\n"}</p>
+                <p>SPD: {stats.spd + "\n"}</p>
+                <p>EVA: {stats.eva + "\n"}</p>
+            </div>
+        )
     }
 
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Choose two numbers to add:
-                </p>
-                n1: <input id="n1" type={"number"} onChange={changeFirst}></input>
-                n2: <input id="n2" type={"number"} onChange={changeSecond}></input>
+                <p>Enter a character name and get stats -></p>
+                name: <input id="n1" type={"text"} onChange={changeName}></input>
+                <button onClick={fetchApi}>Submit</button>
+
                 <br/>
-                <h2 className={"result"}>Result: {answer.result}</h2>
+                {
+                    (character !== {}) ? <div>
+                        <p className={"result"}>Name: {character.name}</p>
+                        <PrettyPrintStats stats={character.stats}/>
+                    </div> : "Loading"
+                }
             </header>
         </div>
     );
