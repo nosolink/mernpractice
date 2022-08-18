d4cbcd84666cb94733ab30e4d4c9146856ff7a11
alachhman <33943685+alachhman@users.noreply.github.com>
2022-08-18 16:21:37 -0400

    improved character generator

--- apipractice/index.js
+++ apipractice/index.js
@@ -33,10 +33,7 @@ app.get('/testWrite', async function (req, res) {
 });
 
 app.get('/testRead', async function (req, res) {
-    const snapshot = await db.collection('users').get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    });
+    const snapshot = await getStatsFromDbByName(req.query.name).get().then(x => x.data())
 
     res.json(snapshot)
 });
@@ -49,58 +46,107 @@ app.get('/add/:n1?/:n2?', function (req, res) {
     res.json( {result: number3});
 });
 
-app.get('/newCharacter/:name?', async function (req, res) {
-    const docRef = db.collection('character').doc(req.query.name);
+app.get('/newCharacter/:name?/:key?', async function (req, res) {
+    const docRef = await getStatsFromDbByName(req.query.name);
 
-    let character = {
-        name: req.query.name,
-        level: 10,
-        currHp: 100,
-        baseStats: {
-            atk: Math.random() * 10,
-            def: Math.random() * 10,
-            spd: Math.random() * 10,
-            eva: Math.random() * 10,
-            maxHp: (Math.random() * 100) + 100,
-        },
-        growthRate: {
-            atk: Math.random(),
-            def: Math.random(),
-            spd: Math.random(),
-            eva: Math.random(),
-            maxHp: Math.random(),
-        },
-        stats: {
-            atk: 0,
-            def: 0,
-            spd: 0,
-            eva: 0,
-            maxHp: 0,
-        }
-    }
+    let character = {};
 
-    let generateStat = (baseStat, growthrate, level) => {
-        let stat = baseStat;
-        for (let i = 0; i < level; i++) {
-            if (Math.random() < growthrate) {
-                stat++;
+    docRef.get().then(async (data) => {
+        if(!data.exists){
+            let character = {
+                name: req.query.name,
+                level: 10,
+                currHp: 100,
+                key: req.query.key,
+                baseStats: {
+                    atk: Math.random() * 10,
+                    def: Math.random() * 10,
+                    spd: Math.random() * 10,
+                    eva: Math.random() * 10,
+                    maxHp: (Math.random() * 100) + 100,
+                },
+                growthRate: {
+                    atk: Math.random(),
+                    def: Math.random(),
+                    spd: Math.random(),
+                    eva: Math.random(),
+                    maxHp: Math.random(),
+                },
+                stats: {
+                    atk: 0,
+                    def: 0,
+                    spd: 0,
+                    eva: 0,
+                    maxHp: 0,
+                },
+                weapon: {
+                    name: getWeaponName(),
+                    stats: {
+                        atk: Math.floor(Math.random() * 10),
+                        def: Math.floor(Math.random() * 10)
+                    }
+                }
+            }
+
+            character.stats.atk = Math.floor(generateStat(character.baseStats.atk, character.growthRate.atk, character.level))
+            character.stats.def = Math.floor(generateStat(character.baseStats.def, character.growthRate.def, character.level))
+            character.stats.spd = Math.floor(generateStat(character.baseStats.spd, character.growthRate.spd, character.level))
+            character.stats.eva = Math.floor(generateStat(character.baseStats.eva, character.growthRate.eva, character.level))
+            character.stats.maxHp = Math.floor(generateStat(character.baseStats.maxHp, character.growthRate.maxHp, character.level))
+
+            await docRef.set(character);
+            await res.send(character)
+
+        } else {
+            if(req.query.key === data.data().key){
+                character = data.data();
+                await res.send(character)
+            } else{
+                await res.send({error: "Invalid Key"})
             }
         }
-        return stat;
-    }
-
-    character.stats.atk = Math.floor(generateStat(character.baseStats.atk, character.growthRate.atk, character.level))
-    character.stats.def = Math.floor(generateStat(character.baseStats.def, character.growthRate.def, character.level))
-    character.stats.spd = Math.floor(generateStat(character.baseStats.spd, character.growthRate.spd, character.level))
-    character.stats.eva = Math.floor(generateStat(character.baseStats.eva, character.growthRate.eva, character.level))
-    character.stats.maxHp = Math.floor(generateStat(character.baseStats.maxHp, character.growthRate.maxHp, character.level))
-
-    await docRef.set(character);
-
-    res.send(character)
+    })
 });
 
 app.get('/', function (req, res) {
     console.log("/home");
     res.send('<h1>home</h1>');
 });
+
+const getStatsFromDbByName = async (name) => {
+    return db.collection('character').doc(name)
+}
+
+let generateStat = (baseStat, growthrate, level) => {
+    let stat = baseStat;
+    for (let i = 0; i < level; i++) {
+        if (Math.random() < growthrate) {
+            stat++;
+        }
+    }
+    return stat;
+}
+
+let getWeaponName = () => {
+    let names = [
+        "Frostward",
+        "Insanity",
+        "Requiem",
+        "Singing Lantern",
+        "Frost Sphere",
+        "Putrid Fan",
+        "Dawne",
+        "Thorn",
+        "Reaper",
+        "Oathkeeper",
+        "Sleepwalker",
+        "Torment",
+        "Prudence",
+        "Flaming Crystal",
+        "Defiled Pouch",
+        "Dreamwhisper",
+        "Earthsong",
+        "Brilliance",
+    ]
+    return names[Math.floor(Math.random() * (names.length))]
+}
--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useEffect} from "react";
+import {Card, Pane} from "evergreen-ui";
 
 function App() {
     // const [numbers, setNumbers] = React.useState({n1: 0, n2: 0});
@@ -42,12 +43,14 @@ function App() {
     //     </div>
     // );
 
-    const [name, setName] = React.useState("Name");
+    const [name, setName] = React.useState("Enter a Name!");
+    const [key, setKey] = React.useState("key");
     const [character, setCharacter] = React.useState(
         {
             name: name,
             level: 10,
             currHp: 100,
+            key: key,
             baseStats: {
                 atk: Math.random() * 10,
                 def: Math.random() * 10,
@@ -68,6 +71,13 @@ function App() {
                 spd: 0,
                 eva: 0,
                 maxHp: 0,
+            },
+            weapon: {
+                name: "Weapon",
+                stats: {
+                    atk: Math.floor(Math.random() * 10),
+                    def: Math.floor(Math.random() * 10)
+                }
             }
         }
     );
@@ -78,7 +88,7 @@ function App() {
     }, [])
 
     const fetchApi = async () => {
-        fetch('http://localhost:8080/newCharacter?name=' + name)
+        fetch('http://localhost:8080/newCharacter?name=' + name + "&key=" + key)
             .then(response => response.json())
             .then(data => setCharacter(data));
     }
@@ -88,15 +98,40 @@ function App() {
         setName(event.target.value);
     }
 
+    const changeKey = (event) => {
+        console.log(event.target.value);
+        setKey(event.target.value);
+    }
+
     const PrettyPrintStats = (props) => {
-        let stats = props.stats;
+        let stats = props.stats.stats;
         return (
-            <div>
-                <p>HP: {stats.maxHp + "\n"}</p>
-                <p>ATK: {stats.atk + "\n"}</p>
-                <p>DEF: {stats.def + "\n"}</p>
-                <p>SPD: {stats.spd + "\n"}</p>
-                <p>EVA: {stats.eva + "\n"}</p>
+            props.stats.error ? "Invalid Key" :
+                <div className={"Test"}>
+                    <p>HP: {stats.maxHp + "\n"}</p>
+                    <div className={"hp-bar"} style={{width: stats.maxHp/200 * 100}}/>
+                    <p>ATK: {stats.atk + "\n"}</p>
+                    <div className={"atk-bar"} style={{width: stats.atk/20 * 100}}/>
+                    <p>DEF: {stats.def + "\n"}</p>
+                    <div className={"def-bar"} style={{width: stats.def/20 * 100}}/>
+                    <p>SPD: {stats.spd + "\n"}</p>
+                    <div className={"spd-bar"} style={{width: stats.spd/20 * 100}}/>
+                    <p>EVA: {stats.eva + "\n"}</p>
+                    <div className={"eva-bar"} style={{width: stats.eva/20 * 100}}/>
+                </div>
+        )
+    }
+
+    const WeaponStats = (props) => {
+        let weapon = props.weapon.weapon
+
+        return (
+            props.weapon.err ? "" : <div className={"Test"} style={{marginTop: "15px"}}>
+                <p><strong>Weapon:</strong> {weapon.name}</p>
+                <p>ATK: {weapon.stats.atk}</p>
+                <div className={"atk-bar"} style={{width: weapon.stats.atk/10 * 100}}/>
+                <p>DEF: {weapon.stats.def}</p>
+                <div className={"def-bar"} style={{width: weapon.stats.def/10 * 100}}/>
             </div>
         )
     }
@@ -104,16 +139,25 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>Enter a character name and get stats -></p>
-                name: <input id="n1" type={"text"} onChange={changeName}></input>
-                <button onClick={fetchApi}>Submit</button>
+                <h3>Generate a Character</h3>
+                <p>
+                    <strong>Name:</strong> <input id="n1" type={"text"} onChange={changeName} className={"Input"}></input>
+                </p>
+                <p>
+                    <strong>Key:</strong> <input id="n1" type={"text"} onChange={changeKey} className={"Input"}></input>
+                </p>
+                <button onClick={fetchApi} className={"submitButton"}>Submit</button>
 
                 <br/>
                 {
-                    (character !== {}) ? <div>
-                        <p className={"result"}>Name: {character.name}</p>
-                        <PrettyPrintStats stats={character.stats}/>
-                    </div> : "Loading"
+                    (character !== {}) ?
+                        <div>
+                            <p className={"result"}>Name: {character.name}</p>
+                            <div className={"divider"}/>
+                            <PrettyPrintStats stats={character}/>
+                            <WeaponStats weapon={character}/>
+                        </div>
+                        : "Loading"
                 }
             </header>
         </div>
